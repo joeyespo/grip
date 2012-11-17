f10f21c4dfee89085dfd4468378f27f5f09acf14
Joe Esposito <joe@joeyespo.com>
2012-11-17 13:37:09 -0500

    Updated config.

--- grip/config.py
+++ grip/config.py
@@ -1,13 +1,13 @@
 """\
-Default Config
+Configuration
 
-Do NOT change the values here.
+Do NOT override the values here.
 Override them using command-line arguments or with a local_config.py instead.
 """
 
 HOST = 'localhost'
-PORT = 8080
-DEBUG = False
+PORT = 5000
+DEBUG = True
 
 STYLE_URLS = [
     'https://a248.e.akamai.net/assets.github.com/assets/github-26581c8456a7088e423db87d6ed68794e9042e29.css',
