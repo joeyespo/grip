db4f148d5a3acb5252498bfbad46934ec74c0e1c
Joe Esposito <joe@joeyespo.com>
2012-11-20 23:32:26 -0500

    Handle port command-line argument

--- grip/command.py
+++ grip/command.py
@@ -1,13 +1,23 @@
+import sys
 from .server import serve
 
 
 def main():
     """The entry point of the command-line interface."""
 
-    # TODO: process arguments
+    # TODO: more advanced argument processing
+
+    # Handle port
+    port = None
+    if len(sys.argv) > 1:
+        port_arg = sys.argv[1]
+        try:
+            port = int(port_arg[1:] if port_arg.startswith(':') else port_arg)
+        except:
+            pass
 
     try:
-        serve()
+        serve(port=port)
     except ValueError, ex:
         # Show input error
         print 'Error:', ex
