861793591fd8a1f86440a28864ec3a35c9970769
Joe Esposito <joe@joeyespo.com>
2012-12-01 18:04:05 -0500

    Re-order setup.py args.

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
+import os
 from setuptools import setup, find_packages
 import grip as package
 
 
 def read(fname):
-    import os
     with open(os.path.join(os.path.dirname(__file__), fname)) as f:
         return f.read()
 
 
 setup(
     name=package.__name__,
+    version=package.__version__,
+    description=package.__description__,
+    long_description=package.__doc__,
     author='Joe Esposito',
     author_email='joe@joeyespo.com',
     url='http://github.com/joeyespo/grip',
     license='MIT',
-    version=package.__version__,
-    description=package.__description__,
-    long_description=package.__doc__,
     platforms='any',
     packages=find_packages(),
     package_data={package.__name__: ['LICENSE', 'static/*', 'templates/*']},
-    entry_points={'console_scripts': ['grip = grip.command:main']},
+    include_package_data=True,
     install_requires=read('requirements.txt'),
+    zip_safe=False,
+    entry_points={'console_scripts': ['grip = grip.command:main']},
 )
