2101556623c5f339edfd7e8fa3b3e9c20b89db92
Joe Esposito <joe@joeyespo.com>
2012-12-08 13:08:56 -0500

    Use path-and-address.

--- grip/command.py
+++ grip/command.py
@@ -17,7 +17,7 @@ Where:
 
 import os
 import sys
-import re
+from path_and_address import resolve, split_address
 from docopt import docopt
 from .server import serve
 from . import __version__
@@ -36,9 +36,9 @@ def main(initial_args=None):
     args = docopt(usage, argv=initial_args, version=version)
 
     # Parse arguments
-    path, address = _resolve_path(args['<path>'], args['<address>'])
-    directory, filename = _split_path(path)
-    host, port = _split_address(address)
+    path, address = resolve(args['<path>'], args['<address>'])
+    host, port = split_address(address)
+    directory, filename = _split(path)
 
     # Validate address
     if address and not host and not port:
@@ -54,22 +54,7 @@ def main(initial_args=None):
     return 0
 
 
-def _resolve_path(path_or_address, address=None):
-    """Returns (path, address) based on consecutive optional arguments, [path] [address]."""
-
-    if path_or_address is None or address is not None:
-        return path_or_address, address
-
-    path = None
-    if not _valid_address(path_or_address) or os.path.exists(path_or_address):
-        path = path_or_address
-    else:
-        address = path_or_address
-
-    return path, address
-
-
-def _split_path(path):
+def _split(path):
     """Returns (directory, filename) from the specified path."""
     if path is None:
         return None, None
@@ -78,64 +63,7 @@ def _split_path(path):
         path += os.path.sep
 
     directory, filename = os.path.split(path)
-
     if filename == '':
         filename = None
 
     return directory, filename
-
-
-def _split_address(address):
-    """Returns (host, port) from the specified address string."""
-    invalid = None, None
-    if not address:
-        return invalid
-
-    components = address.split(':')
-    
-    if len(components) > 2 or not _valid_hostname(components[0]):
-        return invalid
-
-    if len(components) == 2 and not _valid_port(components[1]):
-        return invalid
-
-    if len(components) == 1:
-        components.insert(0 if _valid_port(components[0]) else 1, None)
-
-    host, port = components
-    port = int(port) if port else None
-
-    return host, port
-
-
-def _valid_address(address):
-    """Determines whether the specified address string is valid."""
-    if not address:
-        return False
-
-    components = address.split(':')
-    if len(components) > 2 or not _valid_hostname(components[0]):
-        return False
-
-    if len(components) == 2 and not _valid_port(components[1]):
-        return False
-
-    return True
-
-
-def _valid_hostname(host):
-    """Returns whether the specified string is a valid hostname."""
-    if len(host) > 255:
-        return False
-    if host[-1:] == '.':
-        host = host[:-1]
-    allowed = re.compile('(?!-)[A-Z\d-]{1,63}(?<!-)$', re.IGNORECASE)
-    return all(allowed.match(x) for x in host.split('.'))
-
-
-def _valid_port(port):
-    """Returns whether the specified string is a valid port."""
-    try:
-        return 1 <= int(port) <= 65535
-    except:
-        return False
