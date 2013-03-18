81e7d7d45e71f96d85468737708a31aef939091b
Joe Esposito <joe@joeyespo.com>
2013-03-17 21:08:56 -0400

    Update GitHub CSS file regex.

--- grip/default_config.py
+++ grip/default_config.py
@@ -12,4 +12,4 @@ DEBUG = True
 DEBUG_GRIP = False
 STYLE_URLS = []
 STYLE_URL_SOURCE = 'https://github.com/joeyespo/grip'
-STYLE_URL_RE = '<link.+href=[\'"]?([^\'" >]+)[\'"]?.+media=[\'"]?screen[\'"]?.+rel=[\'"]?stylesheet[\'"]?.+/>'
+STYLE_URL_RE = '<link.+href=[\'"]?([^\'" >]+)[\'"]?.+media=[\'"]?(?:screen|all)[\'"]?.+rel=[\'"]?stylesheet[\'"]?.+/>'
