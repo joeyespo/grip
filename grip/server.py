6e9c3017708aef9f22c5c1ed3cb9dd6d29eb37f8
Ismail Badawi <isbadawi@gmail.com>
2013-03-27 18:57:19 -0400

    Cache retrieved styles to be used offline.

--- grip/server.py
+++ grip/server.py
@@ -2,7 +2,7 @@ import os
 import re
 import errno
 import requests
-from flask import Flask, safe_join, abort
+from flask import Flask, safe_join, abort, url_for
 from .renderer import render_page
 
 
@@ -28,9 +28,11 @@ def serve(path=None, host=None, port=None, gfm=False, context=None,
     # Get styles from style source
     @app.before_first_request
     def retrieve_styles():
-        if not app.config['STYLE_URL_SOURCE'] or not app.config['STYLE_URL_RE']:
+        if not (app.config['STYLE_URL_SOURCE'] and app.config['STYLE_URL_RE'] and app.config['STYLE_CACHE']):
             return
-        styles = _get_styles(app.config['STYLE_URL_SOURCE'], app.config['STYLE_URL_RE'])
+
+        styles = _get_styles(app.config['STYLE_URL_SOURCE'], app.config['STYLE_URL_RE'],
+                             app.config['STYLE_CACHE'])
         app.config['STYLE_URLS'] += styles
         if app.config['DEBUG_GRIP']:
             print ' * Retrieved %s style URL%s' % (len(styles), '' if len(styles) == 1 else 's')
@@ -63,14 +65,18 @@ def serve(path=None, host=None, port=None, gfm=False, context=None,
     # Run local server
     app.run(app.config['HOST'], app.config['PORT'], debug=app.debug, use_reloader=app.config['DEBUG_GRIP'])
 
-
-def _get_styles(source_url, pattern):
+def _get_styles(source_url, pattern, cache_dir):
     """Gets the specified resource and parses all styles in the form of the specified pattern."""
     try:
+        cached_styles = os.listdir(cache_dir)
+        if cached_styles:
+            return [url_for('static', filename=style) for style in cached_styles]
+
         r = requests.get(source_url)
         if not 200 <= r.status_code < 300:
             print ' * Warning: retrieving styles gave status code', r.status_code
-        return re.findall(pattern, r.text)
+        urls = re.findall(pattern, r.text)
+        return _cache_contents(urls, cache_dir)
     except Exception, e:
         print ' * Error: could not retrieve styles:', str(e)
         return []
@@ -91,3 +97,25 @@ def _read_file(filename):
     """Reads the contents of the specified file."""
     with open(filename) as f:
         return f.read()
+
+
+def _write_file(filename, contents):
+    """Creates the specified file and writes the given contents to it."""
+    with open(filename, 'w') as f:
+        f.write(contents.encode('utf-8'))
+
+
+def _cache_contents(urls, cache_dir):
+    """
+    Fetches the given urls and caches their contents in the app's
+    static/ directory, so that flask can serve them locally.
+    Returns the static file urls.
+    """
+    static_urls = []
+    for url in urls:
+        basename = url.rsplit('/', 1)[-1]
+        filename = os.path.join(cache_dir, basename)
+        contents = requests.get(url).text
+        _write_file(filename, contents)
+        static_urls.append(url_for('static', filename=basename))
+    return static_urls
