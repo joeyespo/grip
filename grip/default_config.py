fc0316aaeb336045820b186bea1ecd3560989617
Joe Esposito <joe@joeyespo.com>
2013-09-07 19:58:00 -0400

    Defer creating the style cache.
    This waits until Grip is run to create the style cache
    instead of when it's loading its imports.
    
    This commit also cleans up some of the logic, allowing caching to
    be skipped, and adds a TODO to add an option to clear the cache.

--- grip/default_config.py
+++ grip/default_config.py
@@ -4,17 +4,14 @@ Default Configuration
 Do NOT change the values here for risk of accidentally committing them.
 Override them using command-line arguments or with a local_config.py instead.
 """
-import os
 
 HOST = 'localhost'
 PORT = 5000
 DEBUG = True
 
+
 DEBUG_GRIP = False
 STYLE_URLS = []
 STYLE_URL_SOURCE = 'https://github.com/joeyespo/grip'
 STYLE_URL_RE = '<link.+href=[\'"]?([^\'" >]+)[\'"]?.+media=[\'"]?(?:screen|all)[\'"]?.+rel=[\'"]?stylesheet[\'"]?.+/>'
-
-STYLE_CACHE = os.path.join(os.path.dirname(__file__), 'static')
-if not os.path.exists(STYLE_CACHE):
-    os.mkdir(STYLE_CACHE)
+STYLE_CACHE_DIRECTORY = 'style-cache'
--- grip/server.py
+++ grip/server.py
@@ -2,7 +2,8 @@ import os
 import re
 import errno
 import requests
-from flask import Flask, safe_join, abort, url_for
+from traceback import format_exc
+from flask import Flask, current_app, safe_join, abort, url_for, send_from_directory
 from .renderer import render_page
 
 
@@ -25,17 +26,26 @@ def serve(path=None, host=None, port=None, gfm=False, context=None,
     app.config.from_pyfile('default_config.py')
     app.config.from_pyfile('local_config.py', silent=True)
 
+    # Setup style cache
+    style_urls = app.config['STYLE_URLS']
+    if app.config['STYLE_CACHE_DIRECTORY']:
+        style_cache_path = os.path.join(app.instance_path, app.config['STYLE_CACHE_DIRECTORY'])
+        if not os.path.exists(style_cache_path):
+            os.makedirs(style_cache_path)
+    else:
+        style_cache_path = None
+
     # Get styles from style source
     @app.before_first_request
     def retrieve_styles():
-        if not (app.config['STYLE_URL_SOURCE'] and app.config['STYLE_URL_RE'] and app.config['STYLE_CACHE']):
+        """Retrieves the style URLs from the source and caches them, if requested."""
+        if not app.config['STYLE_URL_SOURCE'] or not app.config['STYLE_URL_RE']:
             return
 
-        styles = _get_styles(app.config['STYLE_URL_SOURCE'], app.config['STYLE_URL_RE'],
-                             app.config['STYLE_CACHE'])
-        app.config['STYLE_URLS'] += styles
-        if app.config['DEBUG_GRIP']:
-            print ' * Retrieved %s style URL%s' % (len(styles), '' if len(styles) == 1 else 's')
+        # Fetch style URLs
+        style_urls.extend(
+            _get_style_urls(app.config['STYLE_URL_SOURCE'],
+                app.config['STYLE_URL_RE'], style_cache_path))
 
     # Set overridden config values
     if host is not None:
@@ -59,28 +69,51 @@ def serve(path=None, host=None, port=None, gfm=False, context=None,
                 return abort(404)
         else:
             text = _read_file(path)
-        return render_page(text, filename, gfm, context, render_offline,
-                           app.config['STYLE_URLS'])
+        return render_page(text, filename, gfm, context, render_offline, style_urls)
+
+    @app.route('/cache/<path:filename>')
+    def render_cache(filename=None):
+        return send_from_directory(style_cache_path, filename)
 
     # Run local server
-    app.run(app.config['HOST'], app.config['PORT'], debug=app.debug, use_reloader=app.config['DEBUG_GRIP'])
+    app.run(app.config['HOST'], app.config['PORT'], debug=app.debug,
+        use_reloader=app.config['DEBUG_GRIP'])
 
 
-def _get_styles(source_url, pattern, cache_dir):
-    """Gets the specified resource and parses all styles in the form of the specified pattern."""
+def _get_style_urls(source_url, pattern, style_cache_path):
+    """Gets the specified resource and parses all style URLs in the form of the specified pattern."""
     try:
-        cached_styles = os.listdir(cache_dir)
-        if cached_styles:
-            return [url_for('static', filename=style) for style in cached_styles]
+        # TODO: Add option to clear the cached styles
+        # Skip fetching styles if there's any already cached
+        if style_cache_path:
+            cached = _get_cached_style_urls(style_cache_path)
+            if cached:
+                return cached
 
+        # Find style URLs
         r = requests.get(source_url)
         if not 200 <= r.status_code < 300:
             print ' * Warning: retrieving styles gave status code', r.status_code
         urls = re.findall(pattern, r.text)
-        return _cache_contents(urls, cache_dir)
-    except Exception, e:
-        print ' * Error: could not retrieve styles:', str(e)
+
+        # Cache the styles
+        if style_cache_path:
+            _cache_contents(urls, style_cache_path)
+            urls = _get_cached_style_urls(style_cache_path)
+
+        return urls
+    except Exception as ex:
+        if current_app.config['DEBUG_GRIP']:
+            print format_exc()
+        else:
+            print ' * Error: could not retrieve styles:', str(ex)
         return []
+
+
+def _get_cached_style_urls(style_cache_path):
+    """Gets the URLs of the cached styles."""
+    cached_styles = os.listdir(style_cache_path)
+    return [url_for('render_cache', filename=style) for style in cached_styles]
 
 
 def _find_file(path):
@@ -106,17 +139,11 @@ def _write_file(filename, contents):
         f.write(contents.encode('utf-8'))
 
 
-def _cache_contents(urls, cache_dir):
-    """
-    Fetches the given urls and caches their contents in the app's
-    static/ directory, so that flask can serve them locally.
-    Returns the static file urls.
-    """
-    static_urls = []
+def _cache_contents(urls, style_cache_path):
+    """Fetches the given URLs and caches their contents in the given directory."""
     for url in urls:
         basename = url.rsplit('/', 1)[-1]
-        filename = os.path.join(cache_dir, basename)
+        filename = os.path.join(style_cache_path, basename)
         contents = requests.get(url).text
         _write_file(filename, contents)
-        static_urls.append(url_for('static', filename=basename))
-    return static_urls
+        print ' * Downloaded', url
