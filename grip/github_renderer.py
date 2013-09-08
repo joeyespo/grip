42fe1bb0af3db57c3aa350a44b6bea66c3b3902a
Joe Esposito <joe@joeyespo.com>
2013-09-08 14:09:40 -0400

    Simplify github_renderer.

--- grip/github_renderer.py
+++ grip/github_renderer.py
@@ -14,8 +14,6 @@ def render_content(text, gfm=False, context=None, username=None, password=None):
         url = 'https://api.github.com/markdown/raw'
         data = text
     headers = {'content-type': 'text/plain'}
-    if username:
-        r = requests.post(url, headers=headers, data=data, auth=(username, password))
-    else:
-        r = requests.post(url, headers=headers, data=data)
+    auth = (username, password) if username else None
+    r = requests.post(url, headers=headers, data=data, auth=auth)
     return r.text
