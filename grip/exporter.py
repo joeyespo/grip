06391630ac44f3ad608c48583dca3e656c4c9f5b
Joe Esposito <joe@joeyespo.com>
2013-09-21 19:03:58 -0400

    Cleanup write_html.

--- grip/exporter.py
+++ grip/exporter.py
@@ -17,16 +17,14 @@ except ImportError:
 
 def write_html(path=None):
     """Exports the rendered HTML to a file."""
+    if not path or os.path.isdir(path):
+        path = _find_file(path)
+
     style_urls = _get_style_urls(config['STYLE_URL_SOURCE'], config['STYLE_URL_RE'])
     style_url_contents = [urlopen(css).read().decode('utf-8') for css in style_urls]
 
-    if not path or os.path.isdir(path):
-        fname_in = _find_file(path)
-        text = _read_file(fname_in)
-        outname = os.path.splitext(fname_in)[0] + '.html'
-    else:
-        text = _read_file(path)
-        outname = os.path.splitext(path)[0] + '.html'
+    text = _read_file(path)
+    outname = os.path.splitext(path)[0] + '.html'
     page = render_page(text=text, style_url_contents=style_url_contents)
 
     with open(outname, 'w') as f:
