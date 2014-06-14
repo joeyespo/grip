2f3dee3978579d89d6164cdc7c95ca024a9d921c
Joe Esposito <joe@joeyespo.com>
2014-06-14 15:48:22 -0400

    Set version to 2.0.1.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def read(fname):
 
 setup(
     name='grip',
-    version='2.0.0',
+    version='2.0.1',
     description='Render local readme files before sending off to Github.',
     long_description=__doc__,
     author='Joe Esposito',
