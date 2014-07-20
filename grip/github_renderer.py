587abec7ff5b90c03885e164d9b6b62a1fb41f76
Joe Esposito <joe@joeyespo.com>
2014-07-20 10:44:50 -0400

    Fix the headers sent by the GitHub renderer.

--- grip/github_renderer.py
+++ grip/github_renderer.py
@@ -11,12 +11,13 @@ def render_content(text, gfm=False, context=None,
         if context:
             data['context'] = context
         data = json.dumps(data)
+        headers = {'content-type': 'application/json'}
     else:
         url = 'https://api.github.com/markdown/raw'
         data = text
-    headers = {'content-type': 'text/plain'}
-    auth = (username, password) if username else None
+        headers = {'content-type': 'text/x-markdown'}
 
+    auth = (username, password) if username else None
     r = requests.post(url, headers=headers, data=data, auth=auth)
 
     # Relay HTTP errors
