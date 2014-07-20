8fa4888dbf82d225f52b6df347372a0381c08237
Joe Esposito <joe@joeyespo.com>
2014-07-20 15:06:24 -0400

    Add __main__.py for running python -m grip.

---
+++ grip/__main__.py
@@ -0,0 +1,15 @@
+"""\
+Grip
+----
+
+Render local readme files before sending off to Github.
+
+:copyright: (c) 2014 by Joe Esposito.
+:license: MIT, see LICENSE for more details.
+"""
+
+from command import main
+
+
+if __name__ == '__main__':
+    main()
