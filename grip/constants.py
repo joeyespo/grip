f0dc0309518e8862ac7ad785d97a485df538b965
Joe Esposito <joe@joeyespo.com>
2015-05-31 15:36:04 -0400

    Fix map resolving to empty list on Python 3.

--- grip/constants.py
+++ grip/constants.py
@@ -3,4 +3,4 @@ supported_extensions = ['.md', '.markdown']
 
 
 # The default filenames when no file is provided
-default_filenames = map(lambda ext: 'README' + ext, supported_extensions)
+default_filenames = ['README' + ext for ext in supported_extensions]
