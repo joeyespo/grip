6c45363ea4f0f32a7980633152315dc8ddeecafd
Joe Esposito <joe@joeyespo.com>
2015-09-20 21:28:50 -0400

    Fix __main__ path:
    * Use abspath since some Python versions use relative URLs for __file__
    * Insert into second path position in case another version of Grip is already on the path

--- grip/__main__.py
+++ grip/__main__.py
@@ -13,7 +13,8 @@ import sys
 
 
 if __name__ == '__main__':
-    sys.path.append(os.path.dirname(__file__))
+    sys.path.insert(1, os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__))))
 
     from grip.command import main
     main()
