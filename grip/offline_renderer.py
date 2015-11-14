e06b8b8533f0008fc9e29ad8b398866ce47bd854
Joe Esposito <joe@joeyespo.com>
2015-11-14 16:13:39 -0500

    Defer markdown import error in the undocumented offline renderer (fixes Python 2.6).

--- grip/offline_renderer.py
+++ grip/offline_renderer.py
@@ -1,11 +1,19 @@
-import markdown
-from .mdx_urlize import UrlizeExtension
+try:
+    import markdown
+    from .mdx_urlize import UrlizeExtension
+except ImportError:
+    markdown = None
+    UrlizeExtension = None
 
 
 def render_content(text, user_content=False, context=None):
     """
     Renders the specified markup locally.
     """
+    if markdown is None:
+        import markdown
+    if UrlizeExtension is None:
+        from .mdx_urlize import UrlizeExtension
     return markdown.markdown(text, extensions=[
         'fenced_code',
         'codehilite(css_class=highlight)',
