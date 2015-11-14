bee9797a046708bf07f69e0596f0a53079a2031b
Joe Esposito <joe@joeyespo.com>
2015-11-14 15:58:06 -0500

    Add tests.

---
+++ tests/test_cli.py
@@ -0,0 +1,49 @@
+"""
+Tests the Grip command-line interface.
+"""
+
+from __future__ import print_function, unicode_literals
+
+from subprocess import PIPE, STDOUT, CalledProcessError, Popen
+
+import pytest
+from grip.command import usage, version
+
+
+def run(*args, **kwargs):
+    command = kwargs.pop('command', 'grip')
+    stdin = kwargs.pop('stdin', None)
+
+    cmd = [command] + list(args)
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT,
+              universal_newlines=True)
+    out1, _ = p.communicate(input=stdin)
+    p.stdin.close()
+    returncode = p.wait()
+    out2, _ = p.communicate()
+    output = out1 + out2
+    if returncode != 0:
+        raise CalledProcessError(returncode, cmd, output=output)
+    return output
+
+
+def test_help():
+    assert run('-h') == usage
+    assert run('--help') == usage
+
+
+def test_version():
+    assert run('-V') == version + '\n'
+    assert run('--version') == version + '\n'
+
+
+def test_bad_command():
+    simple_usage = '\n\n'.join(usage.split('\n\n')[:1])
+    with pytest.raises(CalledProcessError) as excinfo:
+        run('--does-not-exist')
+    assert excinfo.value.output == simple_usage + '\n'
+
+
+# TODO: Figure out how to run the CLI and still capture requests
+# TODO: Test all Grip CLI commands and arguments
+# TODO: Test settings wire-up
