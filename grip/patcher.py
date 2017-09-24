3d04bb1774e286df4cda3695b938251e0a6266ae
Joe Esposito <joe@joeyespo.com>
2017-09-24 11:06:34 -0400

    Patch the GitHub API response to work around the header bug

--- grip/patcher.py
+++ grip/patcher.py
@@ -11,6 +11,11 @@ COMPLETE_TASK_SUB = (r'<li class="task-list-item">'
                      r'checked="" disabled=""> \1\2')
 
 
+HEADER_PATCH_RE = re.compile(r'<span>{:"aria-hidden"=&gt;"true", :class=&gt;'
+                             r'"octicon octicon-link"}</span>', re.DOTALL)
+HEADER_PATCH_SUB = r'<span class="octicon octicon-link"></span>'
+
+
 def patch(html, user_content=False):
     """
     Processes the HTML rendered by the GitHub API, patching
@@ -21,5 +26,9 @@ def patch(html, user_content=False):
     if not user_content:
         html = INCOMPLETE_TASK_RE.sub(INCOMPLETE_TASK_SUB, html)
         html = COMPLETE_TASK_RE.sub(COMPLETE_TASK_SUB, html)
+
+    # FUTURE: Remove this once GitHub API fixes the header bug
+    # https://github.com/joeyespo/grip/issues/244
+    html = HEADER_PATCH_RE.sub(HEADER_PATCH_SUB, html)
 
     return html
